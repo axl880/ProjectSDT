4ca6f6afa3454c0f792ad0e6ec1e146d86bdb1f9
axl880 <165738039+axl880@users.noreply.github.com>
2024-10-05 23:05:03 -0600

    updated

--- add.py
+++ add.py
@@ -4,22 +4,19 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("Vehículos disponibles: 🚗🚙🚐")
-
+#st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+#st.header("Vehículos disponibles: 🚗🚙🚐")
 
 # Title of the app centered
 st.title('US Vehicle Advertisement Listings')
 
-
-
-# Read data from csv file vehicled_us_clean.csv
+# Read file
 df = pd.read_csv('./vehicles_us_ok.csv')
 
-# Show data in the app
+#table in the app
 st.write(df)
 
-# histogram of the types of vehicles by manufacturer
+# histogram  vehicles vs manufacturer
 st.subheader('Histogram of the types of vehicles by manufacturer')
 fig = px.histogram(df, x='manufacturer', color='type')
 # plot the histogram
@@ -33,35 +30,10 @@ manufacturer1 = st.selectbox('Manufacturer 1', df['manufacturer'].unique(), inde
 manufacturer2 = st.selectbox('Manufacturer 2', df['manufacturer'].unique(), index=2)
 # create a normalized histogram checkbox
 normalized = st.checkbox('Normalized')
-# create a histogram with manufacturer1 and manufacturer2 input
-fig = px.histogram()
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer1]['price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer2]['price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
-# normalize the histogram if the checkbox is checked
-if normalized:
-    fig.update_layout(barmode='overlay')
-    fig.update_traces(opacity=0.75)
-# x-axis title
-fig.update_xaxes(title_text='Price')
-# y-axis title
-fig.update_yaxes(title_text='Percentage')
-# plot the histogram
-st.plotly_chart(fig)
 
 
 
-# scatter plot matrix 
-st.subheader('Scatter plot matrix')
-# drop down for each dimension 
-# index 1, 2, and 3 are used to set default values for the drop down menu
-x_axis = st.selectbox('X axis', df.columns, index=1)
-y_axis = st.selectbox('Y axis', df.columns, index=2)
-# drop down for the color
-color = st.selectbox('Color', df.columns, index=3)
-# subheader for the scatter plot matrix that automatically updates
-st.subheader(f'Scatter plot matrix of {x_axis} and {y_axis} by {color}')
-# create the scatter plot matrix
-fig = px.scatter_matrix(df, dimensions=[x_axis, y_axis], color=color)
-# plot the scatter plot matrix
-st.plotly_chart(fig)
+
+
+fig = px.histogram(df, x="manufacturer", color="type", title="Number of cars for each manufacturer")
 
